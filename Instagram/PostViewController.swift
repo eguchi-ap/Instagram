e5d0271f11c060407b05520ca453e11bbadde516
eguchi-ap <t.eguchi@alphapolis.co.jp>
2023-09-01 17:44:01 +0900

    投稿機能の追加

--- Instagram/PostViewController.swift
+++ Instagram/PostViewController.swift
@@ -24,6 +24,40 @@ class PostViewController: UIViewController {
     }
     
     @IBAction func handlePostButton(_ sender: Any) {
+        // jpegに変換
+        let imageData = image.jpegData(compressionQuality: 0.75)
+        
+        // 保存場所
+        let postRef = Firestore.firestore().collection(Const.PostPath).document()
+        let imageRef = Storage.storage().reference().child(Const.ImagePath).child(postRef.documentID + ".jpg")
+        
+        SVProgressHUD.show()
+        
+        // アップロード処理
+        let metadata = StorageMetadata()
+        metadata.contentType = "image/jpeg"
+        imageRef.putData(imageData!, metadata: metadata) { (metadata, error) in
+            if error != nil {
+                print(error!)
+                SVProgressHUD.showError(withStatus: "画像のアップロードが失敗しました")
+                // 投稿処理をキャンセルし、先頭画面に戻る
+                self.view.window?.rootViewController?.dismiss(animated: true, completion: nil)
+                return
+            }
+            
+            // FireStoreに投稿データを保存する
+            let name = Auth.auth().currentUser?.displayName
+            let postDic = [
+                "name": name!,
+                "caption": self.textField.text!,
+                "date": FieldValue.serverTimestamp(),
+            ] as [String: Any]
+            postRef.setData(postDic)
+            
+            SVProgressHUD.showSuccess(withStatus: "投稿しました")
+            self.view.window?.rootViewController?.dismiss(animated: true, completion: nil)
+        }
+
     }
     
     @IBAction func handleCancelButton(_ sender: Any) {
