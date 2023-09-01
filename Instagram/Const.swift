b3a10e09e089eb02366415c898f165454c41a64d
eguchi-ap <t.eguchi@alphapolis.co.jp>
2023-09-01 16:18:31 +0900

    画像を選択、撮影し加工するところまで済み

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by WEBSYSTEM-MAC39 on 2023/09/01.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
--- Instagram/PostViewController.swift
+++ Instagram/PostViewController.swift
@@ -6,24 +6,26 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseFirestore
+import FirebaseStorage
+import SVProgressHUD
 
 class PostViewController: UIViewController {
-
+    var image: UIImage!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        imageView.image = image
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func handlePostButton(_ sender: Any) {
     }
-    */
-
+    
+    @IBAction func handleCancelButton(_ sender: Any) {
+    }
 }
